fd0c7685845bcf2680f6f3e660aaf0e928a3c261
JefersonFreitas27 <jheffygo@gmail.com>
2025-10-19 13:30:21 -0300

    conceitos de functions if e else

---
+++ Conceitos-js/function.js
@@ -0,0 +1,11 @@
+/* Functions/ funções 
+    Trechos de código que é executado SOMENTE quando solicitado*/
+
+    const variavel="maçã"
+    console.log (variavel)
+
+    function nomeNaTela(){
+        console.log ("Jeferson")
+    }
+
+    nomeNaTela()
---
+++ Conceitos-js/if-else.js
@@ -0,0 +1,18 @@
+/* 
+    Controlador de fluxo
+
+    if - (se) 
+    else - (senão)
+
+    Operadores de comparação
+    > maior que
+    < menor que
+    == igual que
+    
+*/
+
+const notadoAluno=10
+const notaParaPassar=7
+
+if(notadoAluno<notaParaPassar){console.log("passou")}
+else{console.log("não passou")}
