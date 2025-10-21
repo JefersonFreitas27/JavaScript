1fc0076301c44361a727452562c45b9f32ead8ee
JefersonFreitas27 <jheffygo@gmail.com>
2025-10-21 20:15:26 -0300

    conceitos de getelementby

---
+++ Conceitos-js/getelementby.js
@@ -0,0 +1,16 @@
+/*document->HTML
+
+
+document.getElementById("id")
+document.getElementsByClassName("class")
+document.getElementsByTagName("tag")
+document.getElementsByName("name")
+
+Ex: const input=document.getElementById("o id do html aqui")
+
+console.log("input")
+
+querySelector("Trás um elemento, o primeiro que encontrar")
+querySelectorAll("Trás todos os elementos que encontrar")
+
+*/
